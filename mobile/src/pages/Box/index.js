9aabad6f57512eea86cde1ecfb2bb2dd7bd43036
Ed Silva <eed.silva@hotmail.com>
2019-04-30 00:50:33 -0700

    displaying list of icons with customized icon, text and date

--- mobile/src/pages/Box/index.js
+++ mobile/src/pages/Box/index.js
@@ -3,6 +3,9 @@ import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import styles from "./styles";
 import AsyncStorage from "@react-native-community/async-storage";
 import api from "../../services/api";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { distanceInWords } from "date-fns";
+import en from "date-fns/locale/en";
 
 const Box = () => {
   const [box, setBox] = useState({});
@@ -18,23 +21,39 @@ const Box = () => {
     fetchFiles().then(b => setBox(b));
   }, []);
 
-//   renderItem = (item) => (
-//     <TouchableOpacity 
-//         onPress={()=>{}}
-//         style={styles.file}
+  onUpload = () => {
 
-//     />
-//   );
+  }
 
+  renderItem = ({item}) => (
+    <TouchableOpacity onPress={() => {}} style={styles.file}>
+      <View style={styles.fileInfo}>
+        <Icon name="insert-drive-file" size={24} color="#A5CFFF" />
+        <Text style={styles.fileTitle}>{item.title}</Text>
+      </View>
+
+      <Text style={styles.fileDate}>
+        {distanceInWords(item.createdAt, new Date(), {
+          locale: en
+        })}
+        {" "} ago
+      </Text>
+    </TouchableOpacity>
+  );
+  
   return (
     <View style={styles.container}>
       <Text style={styles.boxTitle}>{box.title}</Text>
-      {/* <FlatList
+      <FlatList
         data={box.files}
         keyExtractor={file => file._id}
         ItemSeparatorComponent={() => <View style={styles.separator} />}
         renderItem={renderItem}
-      /> */}
+      />
+
+      <TouchableOpacity style={styles.fab} onPress={onUpload}>
+        <Icon name="cloud-upload" size={24} color="#FFF" />
+      </TouchableOpacity>
     </View>
   );
 };
