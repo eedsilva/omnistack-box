7a428706b2be638119630f78988bbedbfa4a264f
Ed Silva <eed.silva@hotmail.com>
2019-04-30 00:28:26 -0700

    initial box screen

--- mobile/src/pages/Box/index.js
+++ mobile/src/pages/Box/index.js
@@ -1,6 +1,42 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import styles from "./styles";
+import AsyncStorage from "@react-native-community/async-storage";
+import api from "../../services/api";
 
-const Box = () => <View />;
+const Box = () => {
+  const [box, setBox] = useState({});
+
+  useEffect(() => {
+    const fetchFiles = async () => {
+      const boxId = await AsyncStorage.getItem("@RocketBox:box");
+      const response = await api.get(`boxes/${boxId}`);
+
+      return response.data;
+    };
+
+    fetchFiles().then(b => setBox(b));
+  }, []);
+
+//   renderItem = (item) => (
+//     <TouchableOpacity 
+//         onPress={()=>{}}
+//         style={styles.file}
+
+//     />
+//   );
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.boxTitle}>{box.title}</Text>
+      {/* <FlatList
+        data={box.files}
+        keyExtractor={file => file._id}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+        renderItem={renderItem}
+      /> */}
+    </View>
+  );
+};
 
 export default Box;
--- mobile/src/pages/Box/styles.js
+++ mobile/src/pages/Box/styles.js
@@ -1,11 +1,66 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, /*Platform*/ } from "react-native";
+// import {
+//   getBottomSpace,
+//   getStatusBarHeight
+// } from "react-native-iphone-x-helper";
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
+    paddingHorizontal: 20,
+    // paddingTop: Platform.OS === "ios" ? getStatusBarHeight() : 0,
+    flex: 1
+  },
+
+  boxTitle: {
+    marginTop: 50,
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#333"
+  },
+
+  list: {
+    marginTop: 30
+  },
+
+  file: {
+    flexDirection: "row",
     alignItems: "center",
-    paddingHorizontal: 30
+    justifyContent: "space-between",
+    paddingVertical: 20
+  },
+
+  separator: {
+    height: 1,
+    backgroundColor: "#EEE"
+  },
+
+  fileInfo: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+
+  fileTitle: {
+    fontSize: 16,
+    color: "#333",
+    marginLeft: 10
+  },
+
+  fileDate: {
+    fontSize: 14,
+    color: "#666"
+  },
+
+  fab: {
+    position: "absolute",
+    right: 30,
+    bottom: 30, //+ getBottomSpace(),
+    width: 60,
+    height: 60,
+    backgroundColor: "#7159c1",
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent: "center"
   }
 });
 
